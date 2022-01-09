d22f8c2218c2f4323828d19aa40f9380ff33bf6b
NiuZian <Nza6920@163.com>
2022-01-10 01:02:08 +0800

    sentinel 持久化配置

--- security-order-api/src/main/java/com/niu/security/orderapi/config/SentinelConfig.java
+++ security-order-api/src/main/java/com/niu/security/orderapi/config/SentinelConfig.java
@@ -1,12 +1,17 @@
 package com.niu.security.orderapi.config;
 
+import com.alibaba.csp.sentinel.datasource.ReadableDataSource;
+import com.alibaba.csp.sentinel.datasource.zookeeper.ZookeeperDataSource;
 import com.alibaba.csp.sentinel.slots.block.RuleConstant;
 import com.alibaba.csp.sentinel.slots.block.degrade.DegradeRule;
 import com.alibaba.csp.sentinel.slots.block.degrade.DegradeRuleManager;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import com.alibaba.csp.sentinel.slots.block.flow.param.ParamFlowRule;
+import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -25,8 +30,51 @@ import java.util.List;
 @Slf4j
 public class SentinelConfig implements ApplicationListener<ContextRefreshedEvent> {
 
+    @Value("${sentinel.zookeeper.address}")
+    private String zkServer;
+
+    @Value("${sentinel.zookeeper.path}")
+    private String zkPath;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    /**
+     * 读取 sentinel 规则
+     *
+     * @PostConstruct 构造函数执行后立马执行
+     * @author [nza]
+     * @createTime 2022/1/10 0:43
+     */
+    //  @PostConstruct
+    public void loadFlowRulesByZookeeper() {
+        log.info("读取配置 sentinel........");
+        // 读取 zookeeper 配置
+        String appPath = zkPath + "/" + appName;
+        ReadableDataSource<String, List<FlowRule>> flowRuleDataSource = new ZookeeperDataSource<>(zkServer, appPath,
+                source -> JSON.parseArray(source, FlowRule.class)
+        );
+        log.info("配置信息: {}", flowRuleDataSource.getProperty());
+        FlowRuleManager.register2Property(flowRuleDataSource.getProperty());
+    }
+
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
+
+        // 读取远程配置
+        loadFlowRulesByZookeeper();
+
+        // 代码硬编码配置
+        //  loadByCode();
+    }
+
+    /**
+     * 代码配置 sentinel
+     *
+     * @author [nza]
+     * @createTime 2022/1/10 0:55
+     */
+    private void loadByCode() {
         log.info("开始配置 sentinel........");
         // 设置流控规则
         FlowRule rule = new FlowRule();
@@ -52,7 +100,7 @@ public class SentinelConfig implements ApplicationListener<ContextRefreshedEvent
         DegradeRuleManager.loadRules(degradeRules);
 
         // 设置参数限流规则
-        //        ParamFlowRule paramFlowRule = new ParamFlowRule();
-        //        paramFlowRule.setResource("getOrderInfo");
+        ParamFlowRule paramFlowRule = new ParamFlowRule();
+        paramFlowRule.setResource("getOrderInfo");
     }
 }
